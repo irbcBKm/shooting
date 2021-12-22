78a5619fb2994b80295637f21bc882968b4e7159
irbcBKm <201237okct@gmail.com>
2021-12-22 23:39:50 +0900

    移動をGetkeycodeからGetAxisに変更 Networkmanagerの追加およびオブジェクトMe(Player)のprefab化

--- Assets/Script/MoveMe.cs
+++ Assets/Script/MoveMe.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MoveMe : MonoBehaviour
 {
-    public float speed;
+    public float speed = 5;
     private Vector3 mouse;
     private Vector3 target;
 
@@ -18,25 +18,10 @@ public class MoveMe : MonoBehaviour
         /*mouse = Input.mousePosition;
         target = Camera.main.ScreenToWorldPoint(new Vector3(mouse.x, mouse.y, 10));
         transform.LookAt(target);*/
-        //左回転
-        if (Input.GetKey (KeyCode.A)) 
-        {
-            transform.Rotate (0.0f,-1.0f,0.0f);
-        }
-        // 右回転
-        if (Input.GetKey (KeyCode.D)) 
-        {
-            transform.Rotate (0.0f,1.0f,0.0f);
-        }
-        // 前進
-        if (Input.GetKey (KeyCode.W)) 
-        {
-            this.transform.Translate (0.0f,0.0f,0.1f);
-        }
-        // 後退
-        if (Input.GetKey (KeyCode.S)) 
-        {
-            this.transform.Translate (0.0f,0.0f,-0.1f);
-        }
+        var moveT = Input.GetAxis("Vertical");
+        var moveR = Input.GetAxis("Horizontal");
+
+        transform.Translate(0f, 0f, moveT *Time.deltaTime* speed);
+        transform.Rotate(0f, moveR, 0f);
     }
 }
